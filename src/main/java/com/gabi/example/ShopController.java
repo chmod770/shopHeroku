879c10a01cb01ed00550e68fdffb724f43fe3190
gabi <gabon95@o2.pl>
2018-10-23 12:51:29 +0200

    initial commit

---
+++ src/main/java/com/gabi/example/ShopController.java
@@ -0,0 +1,12 @@
+package com.gabi.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShopController {
+    @RequestMapping("hi")
+    String hello(){
+        return "Hello";
+    }
+}
