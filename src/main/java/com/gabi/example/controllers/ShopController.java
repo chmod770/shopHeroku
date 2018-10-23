46a39fe265fd70cd6db4aaf7ddbd49f3bbff1a96
gabi <gabon95@o2.pl>
2018-10-23 15:58:57 +0200

    changed controller

--- src/main/java/com/gabi/example/controllers/ShopController.java
+++ src/main/java/com/gabi/example/controllers/ShopController.java
@@ -1,5 +1,6 @@
 package com.gabi.example.controllers;
 
+import com.gabi.example.repositories.ItemRepository;
 import com.gabi.example.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ShopController {
 
     PeopleRepository peopleRepository;
+    ItemRepository itemRepository;
 
     @Autowired
-    public ShopController(PeopleRepository peopleRepository){
+    public ShopController(PeopleRepository peopleRepository, ItemRepository itemRepository){
         this.peopleRepository = peopleRepository;
+        this.itemRepository = itemRepository;
     }
 
     @RequestMapping("hi")
--- src/main/java/com/gabi/example/repositories/ItemRepository.java
+++ src/main/java/com/gabi/example/repositories/ItemRepository.java
@@ -1,4 +1,12 @@
 package com.gabi.example.repositories;
 
-public interface ItemRepository {
+import com.gabi.example.models.Category;
+import com.gabi.example.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Integer>{
+    List<Item> findByIsPromoted(boolean isPromoted);
+    List<Item> findByCategory(Category category);
 }
