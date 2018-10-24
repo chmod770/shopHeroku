ca79ad9004ea85ded0f43af1fe993f49bbee9240
gabi <gabon95@o2.pl>
2018-10-24 13:50:07 +0200

    added items controller2

--- src/main/java/com/gabi/example/controllers/ItemController.java
+++ src/main/java/com/gabi/example/controllers/ItemController.java
@@ -9,9 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/api/items")
 public class ItemController {
 
